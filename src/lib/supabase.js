e9e0148fe69a6bc6710a3797921af4f7b5f53ab4
connor-robinson <ansonchanw@gmail.com>
2025-09-23 20:21:14 +0100

    trying to fix sign in

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY,
-  { auth: { persistSession: true, autoRefreshToken: true } }
-);
+const url = import.meta.env.VITE_SUPABASE_URL;
+const anon = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(url, anon);
