0354089e3c2dc0350fecf4e6b302132ea30bceee
connor-robinson <ansonchanw@gmail.com>
2025-09-24 15:22:28 +0100

    test2

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = import.meta.env.VITE_SUPABASE_URL;
+const url = 'https://bcbttpsokwoapjypwwwq.supabase.co';
 const anon = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(url, anon);
